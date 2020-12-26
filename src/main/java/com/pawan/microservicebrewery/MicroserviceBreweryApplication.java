ce1385fcdab7aad55e9176b6d74d866e2dbb4a4b
iampawands <iam.pawands@gmail.com>
2020-12-26 15:13:20 +0530

    Initial commit

---
+++ src/main/java/com/pawan/microservicebrewery/MicroserviceBreweryApplication.java
@@ -0,0 +1,13 @@
+package com.pawan.microservicebrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceBreweryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceBreweryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pawan/microservicebrewery/MicroserviceBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pawan.microservicebrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceBreweryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
